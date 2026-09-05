9596cd35465540c4b2a518112f9f66188d5c97c5
studentcs502023 <jefersonrojast1101@gmail.com>
2026-09-05 17:09:50 -0500

    agregar assets

---
+++ backend/frontend/aguamonte/api/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const api = axios.create({
+  // En local usará 'http://localhost:3000' desde tu .env.development
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
